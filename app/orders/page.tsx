62d0c037f8688565a01c66cdd25d1b7889281fda
Arrascaeta <facundoarrascaetacba@gmail.com>
2026-07-08 12:44:38 -0300

    version 1.0

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,6 +1,8 @@
 import { getOrders, getProductos } from './actions';
 import OrdersClient from './OrdersClient';
 
+export const dynamic = 'force-dynamic';
+
 export default async function OrdersPage() {
     const [orders, productos] = await Promise.all([getOrders(), getProductos()]);
     return <OrdersClient initialOrders={orders} productos={productos} />;
--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,6 +1,8 @@
 import { getSales, getProductosParaVenta } from './actions';
 import SalesClient from './SalesClient';
 
+export const dynamic = 'force-dynamic';
+
 export default async function VentasPage() {
     const [sales, productos] = await Promise.all([getSales(), getProductosParaVenta()]);
     return <SalesClient initialSales={sales} productos={productos} />;
